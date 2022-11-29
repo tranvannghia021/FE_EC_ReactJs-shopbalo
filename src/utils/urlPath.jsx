0fa1af665cf2e4577fb396d1ab724d99bad388d3
tranvannghia021 <nghiatv1@fireapps.vn>
2022-11-29 15:42:07 +0700

    build

--- src/utils/urlPath.jsx
+++ src/utils/urlPath.jsx
@@ -1,4 +1,4 @@
-export const URL_SERVER = 'http://127.0.0.1:8000';
+export const URL_SERVER = 'https://tranvannghia021-refactored-space-barnacle-x47qgpvr9g4f5j-8080.preview.app.github.dev';
 export const IMG_NOT_FOUND =
   'https://www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg';
 export const CITIES = 'https://raw.githubusercontent.com/tanthuan031/cities/master/cities.json';
